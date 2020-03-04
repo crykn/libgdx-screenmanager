5c494f7d7f5b23fb1475a4216c6767077463d21c
crykn <crykn@users.noreply.github.com>
2020-03-04 22:14:50 +0100

    GL Transitions shader is now compatible with mac.

--- src/main/java/de/eskalon/commons/screen/transition/impl/GLTransitionsShaderTransition.java
+++ src/main/java/de/eskalon/commons/screen/transition/impl/GLTransitionsShaderTransition.java
@@ -44,45 +44,45 @@ import com.badlogic.gdx.math.Interpolation;
  */
 public class GLTransitionsShaderTransition extends ShaderTransition {
 
-	private static final String VERT_SHADER = "#version 330\n#ifdef GL_ES\n" + 
+	private static final String VERT_SHADER = "#version 120\n#ifdef GL_ES\n" + 
 			"precision mediump float;\n" + 
 			"#endif\n" + 
 			"\n" + 
-			"in vec3 a_position;\n" + 
-			"in vec2 a_texCoord0;\n" + 
+			"attribute vec3 a_position;\n" + 
+			"attribute vec2 a_texCoord0;\n" + 
 			"\n" + 
 			"uniform mat4 u_projTrans;\n" + 
 			"\n" + 
-			"out vec3 v_position;\n" + 
-			"out vec2 v_texCoord0;\n" + 
+			"varying vec3 v_position;\n" + 
+			"varying vec2 v_texCoord0;\n" + 
 			"\n" + 
 			"void main() {\n" + 
 			"	v_position = a_position;\n" + 
 			"	v_texCoord0 = a_texCoord0;\n" + 
 			"	gl_Position = u_projTrans * vec4(a_position, 1.0);\n" + 
 			"}";
-	private static final String FRAG_SHADER_PREPEND = "#version 330\n#ifdef GL_ES\n" + 
+	private static final String FRAG_SHADER_PREPEND = "#version 120\n#ifdef GL_ES\n" + 
 			"precision mediump float;\n" + 
 			"#endif\n" + 
 			"\n" + 
-			"in vec3 v_position;\n" + 
-			"in vec2 v_texCoord0;\n" + 
+			"varying vec3 v_position;\n" + 
+			"varying vec2 v_texCoord0;\n" + 
 			"\n" + 
-			"out vec4 out_Color;\n" + 
+			"\n" + 
 			"\n" + 
 			"uniform sampler2D lastScreen;\n" + 
 			"uniform sampler2D currScreen;\n" + 
 			"uniform float progress;\n" + 
 			"\n" + 
 			"vec4 getToColor(vec2 uv) {\n" + 
-			"		return texture(currScreen, uv);\n" + 
+			"		return texture2D(currScreen, uv);\n" + 
 			"}\n" + 
 			"\n" + 
 			"vec4 getFromColor(vec2 uv) {\n" + 
-			"		return texture(lastScreen, uv);\n" + 
+			"		return texture2D(lastScreen, uv);\n" + 
 			"}\n";
 	private static final String FRAG_SHADER_POSTPEND = "\nvoid main() {\n" + 
-		"	out_Color = transition(v_texCoord0);\n" + 
+		"	gl_FragColor = transition(v_texCoord0);\n" + 
 		"}\n";
 	
 	/**
