f286f7eb7890de1f09b55e7404f52a78a29f2ff7
damios <crykn@users.noreply.github.com>
2020-03-21 17:21:50 +0100

    Adds support for the OpenGL 3.2 core profile on Mac OS. (#3)

--- src/main/java/de/eskalon/commons/screen/transition/impl/GLTransitionsShaderTransition.java
+++ src/main/java/de/eskalon/commons/screen/transition/impl/GLTransitionsShaderTransition.java
@@ -17,8 +17,10 @@ package de.eskalon.commons.screen.transition.impl;
 
 import javax.annotation.Nullable;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Interpolation;
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
 
 /**
  * A transition that is using shader code conforming to the GL Transition
@@ -42,90 +44,47 @@ import com.badlogic.gdx.math.Interpolation;
  *      information on the GL Transition spec</a>
  */
 public class GLTransitionsShaderTransition extends ShaderTransition {
-	
+
 	// @formatter:off
-	private static final String VERT_SHADER = "#version 120\n" +
-			"#ifdef GL_ES\n" + 
-			"precision mediump float;\n" + 
-			"#endif\n" + 
-			"\n" + 
-			"attribute vec3 a_position;\n" + 
-			"attribute vec2 a_texCoord0;\n" + 
-			"\n" + 
-			"uniform mat4 u_projTrans;\n" + 
-			"\n" + 
-			"varying vec3 v_position;\n" + 
-			"varying vec2 v_texCoord0;\n" + 
-			"\n" + 
-			"void main() {\n" + 
-			"	v_position = a_position;\n" + 
-			"	v_texCoord0 = a_texCoord0;\n" + 
-			"	gl_Position = u_projTrans * vec4(a_position, 1.0);\n" + 
-			"}";
-//	private static final String VERT_SHADER_GLSL_150 = "#version 150\n#ifdef GL_ES\n" + 
-//			"precision mediump float;\n" + 
-//			"#endif\n" + 
-//			"\n" + 
-//			"in vec3 a_position;\n" + 
-//			"in vec2 a_texCoord0;\n" + 
-//			"\n" + 
-//			"uniform mat4 u_projTrans;\n" + 
-//			"\n" + 
-//			"out vec3 v_position;\n" + 
-//			"out vec2 v_texCoord0;\n" + 
-//			"\n" + 
-//			"void main() {\n" + 
-//			"	v_position = a_position;\n" + 
-//			"	v_texCoord0 = a_texCoord0;\n" + 
-//			"	gl_Position = u_projTrans * vec4(a_position, 1.0);\n" + 
-//			"}";
-	private static final String FRAG_SHADER_PREPEND = "#version 120\n" +
-			"#ifdef GL_ES\n" +  
-			"precision mediump float;\n" + 
-			"#endif\n" + 
-			"\n" + 
-			"varying vec3 v_position;\n" + 
-			"varying vec2 v_texCoord0;\n" + 
-			"\n" + 
-			"\n" + 
-			"\n" + 
-			"uniform sampler2D lastScreen;\n" + 
-			"uniform sampler2D currScreen;\n" + 
-			"uniform float progress;\n" + 
-			"\n" + 
-			"vec4 getToColor(vec2 uv) {\n" + 
-			"		return texture2D(currScreen, uv);\n" + 
-			"}\n" + 
-			"\n" + 
-			"vec4 getFromColor(vec2 uv) {\n" + 
-			"		return texture2D(lastScreen, uv);\n" + 
-			"}\n";
-//	private static final String FRAG_SHADER_PREPEND_GLSL_150 = "#version 150\n#ifdef GL_ES\n" + 
-//			"precision mediump float;\n" + 
-//			"#endif\n" + 
-//			"\n" + 
-//			"in vec3 v_position;\n" + 
-//			"in vec2 v_texCoord0;\n" + 
-//			"\n" + 
-//			"out vec4 out_Color;\n" + 
-//			"\n" + 
-//			"uniform sampler2D lastScreen;\n" + 
-//			"uniform sampler2D currScreen;\n" + 
-//			"uniform float progress;\n" + 
-//			"\n" + 
-//			"vec4 getToColor(vec2 uv) {\n" + 
-//			"		return texture(currScreen, uv);\n" + 
-//			"}\n" + 
-//			"\n" + 
-//			"vec4 getFromColor(vec2 uv) {\n" + 
-//			"		return texture(lastScreen, uv);\n" + 
-//			"}\n";
-	private static final String FRAG_SHADER_POSTPEND = "\nvoid main() {\n" + 
-		"	gl_FragColor = transition(v_texCoord0);\n" + 
-		"}\n";
-//	private static final String FRAG_SHADER_POSTPEND_GLSL_150 = "\nvoid main() {\n" + 
-//			"	out_Color = transition(v_texCoord0);\n" + 
-//			"}\n";
+	private static final String VERT_SHADER = "#version 120\n"
+			+ "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n"
+			+ "\n" + "attribute vec3 a_position;\n"
+			+ "attribute vec2 a_texCoord0;\n" + "\n"
+			+ "uniform mat4 u_projTrans;\n" + "\n"
+			+ "varying vec3 v_position;\n" + "varying vec2 v_texCoord0;\n"
+			+ "\n" + "void main() {\n" + "	v_position = a_position;\n"
+			+ "	v_texCoord0 = a_texCoord0;\n"
+			+ "	gl_Position = u_projTrans * vec4(a_position, 1.0);\n" + "}";
+	private static final String VERT_SHADER_GLSL_150 = "#version 150\n#ifdef GL_ES\n"
+			+ "precision mediump float;\n" + "#endif\n" + "\n"
+			+ "in vec3 a_position;\n" + "in vec2 a_texCoord0;\n" + "\n"
+			+ "uniform mat4 u_projTrans;\n" + "\n" + "out vec3 v_position;\n"
+			+ "out vec2 v_texCoord0;\n" + "\n" + "void main() {\n"
+			+ "	v_position = a_position;\n" + "	v_texCoord0 = a_texCoord0;\n"
+			+ "	gl_Position = u_projTrans * vec4(a_position, 1.0);\n" + "}";
+	private static final String FRAG_SHADER_PREPEND = "#version 120\n"
+			+ "#ifdef GL_ES\n" + "precision mediump float;\n" + "#endif\n"
+			+ "\n" + "varying vec3 v_position;\n"
+			+ "varying vec2 v_texCoord0;\n" + "\n" + "\n" + "\n"
+			+ "uniform sampler2D lastScreen;\n"
+			+ "uniform sampler2D currScreen;\n" + "uniform float progress;\n"
+			+ "\n" + "vec4 getToColor(vec2 uv) {\n"
+			+ "		return texture2D(currScreen, uv);\n" + "}\n" + "\n"
+			+ "vec4 getFromColor(vec2 uv) {\n"
+			+ "		return texture2D(lastScreen, uv);\n" + "}\n";
+	private static final String FRAG_SHADER_PREPEND_GLSL_150 = "#version 150\n#ifdef GL_ES\n"
+			+ "precision mediump float;\n" + "#endif\n" + "\n"
+			+ "in vec3 v_position;\n" + "in vec2 v_texCoord0;\n" + "\n"
+			+ "out vec4 out_Color;\n" + "\n" + "uniform sampler2D lastScreen;\n"
+			+ "uniform sampler2D currScreen;\n" + "uniform float progress;\n"
+			+ "\n" + "vec4 getToColor(vec2 uv) {\n"
+			+ "		return texture(currScreen, uv);\n" + "}\n" + "\n"
+			+ "vec4 getFromColor(vec2 uv) {\n"
+			+ "		return texture(lastScreen, uv);\n" + "}\n";
+	private static final String FRAG_SHADER_POSTPEND = "\nvoid main() {\n"
+			+ "	gl_FragColor = transition(v_texCoord0);\n" + "}\n";
+	private static final String FRAG_SHADER_POSTPEND_GLSL_150 = "\nvoid main() {\n"
+			+ "	out_Color = transition(v_texCoord0);\n" + "}\n";
 	// @formatter:on
 
 	/**
@@ -149,9 +108,20 @@ public class GLTransitionsShaderTransition extends ShaderTransition {
 	 *            uncomment/set the transition parameters!
 	 */
 	public void compileGLTransition(String glTransitionsCode) {
-		compileShader(VERT_SHADER,
-				FRAG_SHADER_PREPEND + glTransitionsCode + FRAG_SHADER_POSTPEND,
-				true);
+		// TODO use PlatformUtils.isMac (see
+		// https://github.com/libgdx/libgdx/pull/5960)
+
+		if (Gdx.gl30 != null && UIUtils.isMac) {
+			// Mac only supports the OpenGL 3.2 core profile, which is not
+			// backward compatible
+			compileShader(
+					VERT_SHADER_GLSL_150, FRAG_SHADER_PREPEND_GLSL_150
+							+ glTransitionsCode + FRAG_SHADER_POSTPEND_GLSL_150,
+					true);
+		} else {
+			compileShader(VERT_SHADER, FRAG_SHADER_PREPEND + glTransitionsCode
+					+ FRAG_SHADER_POSTPEND, true);
+		}
 	}
 
 }
